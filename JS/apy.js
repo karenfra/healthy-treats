118b23f3929bbd189b13e3bbdc57b14184e1c43f
karen <karen-f_a@hotmail.com>
2025-10-07 15:31:22 -0500

    comit

--- JS/apy.js
+++
@@ -1,5 +0,0 @@
-const map = L.map('map').setView([-12.1614, -76.9715], 15);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap'
-    }).addTo(map);
-    L.marker([-12.1614, -76.9715]).addTo(map).bindPopup("Pasteleria Healthy Treats").openPopup();
